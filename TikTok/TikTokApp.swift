12059ede248c99d08e9c657332f290ec16af1120
Aref <devfera14@gmail.com>
2024-07-22 11:47:15 -0300

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Aref Chucri on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
